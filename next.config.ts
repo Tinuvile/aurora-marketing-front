94c6ac48925a4de44405bee34c58630b38bb62fb
Tinuvile <3145593233@qq.com>
2025-12-03 18:27:28 +0800

    dev
    Signed-off-by: Tinuvile <3145593233@qq.com>

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  env: {
+    API_HOST_URL: process.env.API_HOST_URL,
+    API_POST_ID: process.env.API_POST_ID,
+  },
 };
 
 export default nextConfig;
